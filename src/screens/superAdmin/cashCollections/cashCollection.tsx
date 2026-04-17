33898d788e5c3db58427a9f20609de30c8307ecf
Adnan Ali <adnanalikunbher3@gmail.com>
2026-04-17 16:26:26 +0500

    new changes

--- src/screens/superAdmin/cashCollections/cashCollection.tsx
+++ src/screens/superAdmin/cashCollections/cashCollection.tsx
@@ -46,6 +46,9 @@ interface MachineReport {
     type: string;
     percentage: string;
   };
+  CashToBeReceived?:{
+    CashToBeCollected:string
+  }
 }
 
 interface DailyData {
@@ -936,6 +939,9 @@ const SuperAdminCashCollectionPage: React.FC = () => {
                             Difference
                           </th>
                           <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Cash In Machine
+                          </th>
+                          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Status
                           </th>
                           <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -1021,6 +1027,12 @@ const SuperAdminCashCollectionPage: React.FC = () => {
                                       ({machine.difference.percentage})
                                     </p>
                                   )}
+                                </td>
+                                
+                                <td className="px-6 py-4 whitespace-nowrap text-sm">
+                                  <span className={`inline-flex items-center gap-1 px-2.5 py-0.5 rounded-full text-xs font-medium`}>
+                                    Rs: {machine.CashToBeReceived?.CashToBeCollected || 0}
+                                  </span>
                                 </td>
                                 <td className="px-6 py-4 whitespace-nowrap text-sm">
                                   <span className={`inline-flex items-center gap-1 px-2.5 py-0.5 rounded-full text-xs font-medium ${diffBgColor} ${diffColor}`}>
